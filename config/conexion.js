d03752db2aa57f7b601ad6614845587653ebacfc
alexmunozca <alexmunozca@gmail.com>
2022-11-18 18:19:30 -0500

    Configuracion conexion Railway

--- config/conexion.js
+++ config/conexion.js
@@ -1,10 +1,17 @@
 var mysql = require('mysql');
 
+const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_USER = process.env.DB_USER || 'root'
+const DB_PASSWORD = process.env.DB_PASSWORD || 'Amc$0629$'
+const DB_NAME = process.env.DB_NAME || 'baseCrud'
+const DB_PORT = process.env.DB_PORT || 3306
+
 var baseArticulos = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'Amc$0629$',
-      database: 'baseCrud',
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      port: DB_PORT
    }
 );
 
